0726c756aa102288cf5ff42b778a3fc38a92a039
Msuphap <suphap.ds@gmail.com>
2019-10-20 11:28:45 -0700

    Prepared to deploy again

--- server.js
+++ server.js
@@ -36,5 +36,6 @@ if (process.env.NODE_ENV === 'production'){
   })
 }
 
-const port = 7000;
+// const port = 7000;
+const port = process.env.PORT || 7000;
 app.listen(port, ()=> console.log(`Server running on port ${port}`));
