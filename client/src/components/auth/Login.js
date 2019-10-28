2324a6107cbc5d252241d312d9a914be9d821ad9
Msuphap <suphap.ds@gmail.com>
2019-10-27 20:04:01 -0700

    Final version

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import classnames from 'classnames';
-import {connect} from 'react-redux';
-import {loginUser} from '../../actions/authActions';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/authActions';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class Login extends Component {
   constructor() {
@@ -15,30 +15,36 @@ class Login extends Component {
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
+
   onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
   }
 
   onSubmit(e) {
     e.preventDefault();
 
-    const newUser = {
+    const currentUser = {
       email: this.state.email,
       password: this.state.password
-    };
-
-  this.props.loginUser(newUser);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.auth.isAuthenticated){
-      this.props.history.push('/dashbosrd');
     }
-    if (nextProps.errors) {
-      this.setState({ errors: nextProps.errors });
-    }
+
+    this.props.loginUser(currentUser);
   }
 
   render() {
@@ -49,31 +55,34 @@ class Login extends Component {
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
-              <p className="lead text-center">Sign in to your DevConnector account</p>
-              <form noValidate onSubmit={this.onSubmit}>
-                <div className="form-group">
-                  <input type="email" className={classnames('form-control form-control-lg', {
-                    'is-invalid': errors.email
-                  })} placeholder="Email Address" value={this.state.email} onChange={this.onChange} name="email" />
-                  {errors.email && (
-                    <div className="invalid-feedback">{errors.email}</div>
-                  )}
-                </div>
-                <div className="form-group">
-                  <input type="password" className={classnames('form-control form-control-lg', {
-                    'is-invalid': errors.password
-                  })} placeholder="Password" value={this.state.password} onChange={this.onChange} name="password" />
-                  {errors.password && (
-                    <div className="invalid-feedback">{errors.password}</div>
-                  )}
-                </div>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="Email Address"
+                  name="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  error={errors.email}
+                />
+
+                <TextFieldGroup
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  error={errors.password}
+                />
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -83,9 +92,9 @@ Login.propTypes = {
   errors: PropTypes.object.isRequired
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   auth: state.auth,
   errors: state.errors
 });
 
-export default connect(mapStateToProps, {loginUser})(Login);
+export default connect(mapStateToProps, { loginUser })(Login);
